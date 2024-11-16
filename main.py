a633fbbaffabb8b2aa91319167c6bf0c7d43ce61
Álvaro Troya <alvaro.troya.ec@googlemail.com>
2024-11-16 19:47:57 +0100

    Extract bootstrapping logic into methods

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 import os
 import sqlite3
 import subprocess
+import sys
 
-create_filtered_table = """
-            CREATE TABLE filtered AS
-            SELECT titles.tconst, titleType, primaryTitle, startYear,
-                   runtimeMinutes, genres, averageRating, numVotes
-            FROM titles
-            JOIN ratings ON ratings.tconst = titles.tconst
-            WHERE isAdult = 0 AND titleType = 'movie'
-                  AND averageRating > 5 AND numVotes > 100;"""
 
-sql_code = []
+def log(msg: str) -> None:
+    # TODO: Add rich logging in the future
+    print(msg)
 
-if __name__ == "__main__":
+
+def bootstrap_database():
+    # TODO: Check that directory structure is provided
+    # TODO: Check that TSV files exist
+    # TODO: Use pathlib for this
+
     if not os.path.isfile("assets/database/movies.db"):
-        subprocess.call(
+        log("Setting up DB...")
+        subprocess.run(
             [
                 "sqlite3",
                 "assets/database/movies.db",
@@ -23,8 +24,26 @@ if __name__ == "__main__":
                 ".separator '\t' '\n'",
                 ".import assets/dataset/title.basics.tsv titles",
                 ".import assets/dataset/title.ratings.tsv ratings",
-            ]
+            ],
+            check=True,
+            stdout=sys.stdout,
+            stderr=sys.stderr,
         )
+
+    create_filtered_table()
+
+
+def create_filtered_table():
+    create_filtered_table_sql = """
+            CREATE TABLE filtered AS 
+            SELECT titles.tconst, titleType, primaryTitle, startYear,
+                   runtimeMinutes, genres, averageRating, numVotes
+            FROM titles
+            JOIN ratings ON ratings.tconst = titles.tconst
+            WHERE isAdult = 0 AND titleType = 'movie'
+                  AND averageRating > 5 AND numVotes > 100;"""
+
+    log("Connecting to DB...")
     with sqlite3.connect("assets/database/movies.db") as conn:
         cursor = conn.cursor()
         # check if filtered table exists in the database, else create it
@@ -32,7 +51,9 @@ if __name__ == "__main__":
             """SELECT name FROM sqlite_master
                WHERE type='table' AND name='filtered';"""
         ).fetchall():
-            cursor.execute(create_filtered_table)
-        for command in sql_code:
-            cursor.execute(command)
+            cursor.execute(create_filtered_table_sql)
         conn.commit()
+
+
+if __name__ == "__main__":
+    bootstrap_database()
