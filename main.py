9d700e339fc0b38bc2022c5a7d4011ca0167144d
Álvaro Troya <alvaro.troya.ec@googlemail.com>
2024-11-16 19:21:52 +0100

    Remove typer

--- main.py
+++ main.py
@@ -1,29 +1,6 @@
-import typer
-import sqlite3
-
-# import sqlalchemy as sa
-import subprocess
 import os
-
-### TYPER STUFF ###
-app = typer.Typer()
-
-
-@app.command()
-def hello(name: str):
-    print(f"Hello {name} from zap!")
-
-
-@app.command()
-def goodbye(name: str, formal: bool = False):
-    if formal:
-        print(f"Goodbye Ms. {name}. Have a good day.")
-    else:
-        print(f"Bye {name}")
-
-
-###################
-
+import sqlite3
+import subprocess
 
 create_filtered_table = """
             CREATE TABLE filtered AS
