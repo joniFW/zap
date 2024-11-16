3eafadb6052296cbdf8eff7f4bfb1eea6a70114f
joniFW <jonnymicah@gmail.com>
2024-11-16 17:08:27 +0100

    bootstrapping SQL databse

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import typer
+import sqlite3
+
+# import sqlalchemy as sa
+import subprocess
+import os
 
 app = typer.Typer()
 
@@ -16,6 +21,39 @@ def goodbye(name: str, formal: bool = False):
         print(f"Bye {name}")
 
 
+create_filtered_table = """
+            CREATE TABLE filtered AS
+            SELECT titles.tconst, titleType, primaryTitle, startYear,
+                   runtimeMinutes, genres, averageRating, numVotes
+            FROM titles
+            JOIN ratings ON ratings.tconst = titles.tconst
+            WHERE isAdult = 0 AND titleType = 'movie'
+                  AND averageRating > 5 AND numVotes > 100;"""
+
+sql_code = []
+
 if __name__ == "__main__":
-    # app()
-    pass
+    if not os.path.isfile("assets/database/movies.db"):
+        subprocess.call(
+            [
+                "sqlite3",
+                "assets/database/movies.db",
+                ".mode ascii",
+                ".separator '\t' '\n'",
+                ".import assets/dataset/title.basics.tsv titles",
+                ".import assets/dataset/title.ratings.tsv ratings",
+            ]
+        )
+    with sqlite3.connect("assets/database/movies.db") as conn:
+        cursor = conn.cursor()
+        # check if filtered table exists in the database, else create it
+        if cursor.execute(
+            """SELECT name FROM sqlite_master
+                    WHERE type='table' AND name='filtered';"""
+        ).fetchall():
+            print("The table exists already")
+        else:
+            cursor.execute(create_filtered_table)
+        for command in sql_code:
+            cursor.execute(command)
+        conn.commit()
